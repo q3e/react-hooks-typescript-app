1a62685e2c6fbf6fde411e01e1acfeb444f10090
Fat4 <fat4@kens-MacBook-Air.local>
2019-05-14 07:34:49 +0300

    add ability to import sass & restructure folders

---
+++ src/components/App/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from '../../styles/logo.svg';
+import '../../styles/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Poultry Farming Cost and Profit Calculator
+        </p>
+        <input />
+      </header>
+    </div>
+  );
+}
+
+export default App;
