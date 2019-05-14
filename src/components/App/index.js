9a75c2c3d9fde14b2df57c2341baf7930036282c
Fat4 <fat4@kens-MacBook-Air.local>
2019-05-14 08:10:57 +0300

    import bulma sass correctly + remove some boilerplates

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import logo from '../../styles/logo.svg';
 import '../../styles/App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Poultry Farming Cost and Profit Calculator
         </p>
-        <input />
+        <input class="input is-rounded" type="text" placeholder="Rounded input" />
       </header>
     </div>
   );
